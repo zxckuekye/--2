8447ab437f78de2dc4d10653957993d5203b8aab
zxckuekye <zzxcursed2000@gmail.com>
2026-09-21 15:54:31 +0300

    написан код

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+// Находим необходимые элементы
 const nameInput = document.querySelector('.name-input');
 const btnChangeName = document.querySelector('.btn-change-name');
 const profileName = document.querySelector('.profile-name');
 const btnTheme = document.querySelector('.btn-theme');
 
+// Обновляем имя в карточке профиля через textContent и input.value
 btnChangeName.addEventListener('click', function () {
   profileName.textContent = nameInput.value;
 });
 
+// Переключаем CSS-класс темы 'dark' для body
 btnTheme.addEventListener('click', function () {
   document.body.classList.toggle('dark');
 });
