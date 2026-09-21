6667d73abc1255f1d6c9b7542bc701d7c3cfb82a
zxckuekye <zzxcursed2000@gmail.com>
2026-09-21 15:57:02 +0300

    удалены лишние символы в коде, исправлены ошибки в тексте и комментариях, улучшена читаемость кода.

--- index.js
+++ index.js
@@ -4,12 +4,10 @@ const btnChangeName = document.querySelector('.btn-change-name');
 const profileName = document.querySelector('.profile-name');
 const btnTheme = document.querySelector('.btn-theme');
 
-// Обновляем имя в карточке профиля через textContent и input.value
 btnChangeName.addEventListener('click', function () {
   profileName.textContent = nameInput.value;
 });
 
-// Переключаем CSS-класс темы 'dark' для body
 btnTheme.addEventListener('click', function () {
   document.body.classList.toggle('dark');
 });
